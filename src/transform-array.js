366b1dc90a23f70397828feb9328173e9c00e8c5
ixakina <ixakina@mail.ru>
2021-09-28 12:35:05 +0300

    feat: add task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,19 +1,36 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let res = [];
+  arr.forEach((item, i, arr) => {
+    if (item == "--double-next") {
+      res.push(arr[i + 1]);
+    } else if (item == "--double-prev") {
+      res.push(arr[i - 1]);
+    } else if (item == "--discard-prev") {
+      res.pop(arr[i - 1]);
+    } else res.push(item);
+  });
+  res.forEach((item, i, arr) => {
+    if (item == "--discard-next") {
+      arr.splice(i, 2);
+    }
+  });
+  return res;
 }
